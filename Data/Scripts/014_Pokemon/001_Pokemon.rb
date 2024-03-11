aa1603333a3c4de8599496e8995cbabf7bc77a9d
BinaryElement <33382750+BinaryElement@users.noreply.github.com>
2024-03-11 13:27:54 -0400

    Randomized stat inheritance
    Fused pokemon stats are now calculated by a (seeded-by-ID) random factor

--- Data/Scripts/014_Pokemon/001_Pokemon.rb
+++ Data/Scripts/014_Pokemon/001_Pokemon.rb
@@ -1213,6 +1213,44 @@ class Pokemon
     base_stats = self.baseStats
     this_level = self.level
     this_IV = self.calcIV
+	
+	# TODO setting for override
+	# TODO setting for factors
+	# TODO setting for seed offset
+	
+	# For each stat, a factor is calculated.  The factor determines how much of the stronger stat is used relative to the weaker stat.
+	# For example, a stat factor of 0.75 (and a maximum factor of 1) results in the stat equaling 75% of the pokemon with the stronger stat's, plus 25% of the weaker pokemon.
+	fused_stat_factor_minimum = 0.5
+	fused_stat_factor_maximum = 1.5
+	
+	# If this stat is changed from 1, fusions can be drastically stronger or weaker than their unfused counterparts.
+	# The factor above is used as a ratio against the combined factor.  With a combined factor of 1, the percentages from the above calculation will add up to 100%.  However, with a combined factor of 2 for example, the percentages add up to 200%.
+	# As an example, if a factor is calculated at 1.25, and the combined factor is 2, your pokemon's stat will be 125% of the stronger pokemon's plus 75% of the weaker (calculated from 2 - 1.25).
+	# For another, if the factor is calculated at 0.75 and the combined factor is 1.25, your pokemon's stat will be 75% of the stronger pokemon's plus 50% of the weaker (calculated from 1.25 - 0.75). 
+	fused_stat_factor_combined_minimum = 1.25
+	fused_stat_factor_combined_maximum = 2
+	
+	head_id = getHeadID(species)
+	body_id = getBodyID(species)
+	stat_rand = Random.new( (head_id) + (10000+body_id) + 643 ) #seed offset 643
+	
+	combined_stat_factor = fused_stat_factor_combined_maximum == fused_stat_factor_combined_minimum ? fused_stat_factor_combined_maximum : stat_rand.rand(fused_stat_factor_combined_maximum - fused_stat_factor_combined_minimum) + fused_stat_factor_combined_minimum
+	stat_factor = fused_stat_factor_maximum == fused_stat_factor_minimum ? fused_stat_factor_maximum : stat_rand.rand(fused_stat_factor_maximum - fused_stat_factor_minimum) + fused_stat_factor_minimum
+	if stat_factor > combined_stat_factor
+		stat_factor = combined_stat_factor
+	end
+	
+    if isFusion?()
+		head_species_data = GameData::Species.get(getHeadID(species)).base_stats
+		body_species_data = GameData::Species.get(getBodyID(species)).base_stats
+		GameData::Stat.each_main do |s|
+			if head_species_data[s.id] > body_species_data[s.id]
+				base_stats[s.id] = (stat_factor * head_species_data[s.id]) + ((combined_stat_factor - stat_factor) * body_species_data[s.id])
+			else
+				base_stats[s.id] = (stat_factor * body_species_data[s.id]) + ((combined_stat_factor - stat_factor) * head_species_data[s.id])
+			end
+		end
+	end
 
     if $game_switches[SWITCH_NO_LEVELS_MODE]
       this_level = adjust_level_for_base_stats_mode()
