a8650a5eac8264d2f81dcfd1b9a6c77e3ce12c3d
BinaryElement <33382750+BinaryElement@users.noreply.github.com>
2024-03-11 11:32:40 -0400

    Init
    I don't know why it thinks things have changed

---
+++ INSTALL_OR_UPDATE.bat
@@ -0,0 +1,15 @@
+@echo off
+
+.\REQUIRED_BY_INSTALLER_UPDATER\7z.exe e -spf -aoa "REQUIRED_BY_INSTALLER_UPDATER\MinGit.7z"
+
+set mgit=".\REQUIRED_BY_INSTALLER_UPDATER\cmd\git.exe"
+%mgit% init .
+%mgit% remote add origin "https://github.com/infinitefusion/infinitefusion-e18.git"
+%mgit% fetch origin releases
+%mgit% reset --hard origin/releases
+
+echo:
+echo Installer/Updater made by Hungry Pickle
+echo:
+echo Thank you for installing or updating! You can close this window now.
+pause
