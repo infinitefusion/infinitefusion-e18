2ad4298ddcb9b4f7be7caca4cc951f9a0550b2af
bentuczinski-hue <bentuczinski@gmail.com>
2025-11-21 01:45:43 -0500

    Update 001_Settings.rb

--- Data/Scripts/001_Settings.rb
+++ Data/Scripts/001_Settings.rb
@@ -159,7 +159,7 @@ module Settings
   # Number of badges in the game
   NB_BADGES = 16
   # The odds of a newly generated Pokémon being shiny (out of 65536).
-  SHINY_POKEMON_CHANCE = 16#(MECHANICS_GENERATION >= 6) ? 16 : 8
+  SHINY_POKEMON_CHANCE = 256#(MECHANICS_GENERATION >= 6) ? 16 : 8
 
   # The odds of a wild Pokémon/bred egg having Pokérus (out of 65536).
   POKERUS_CHANCE = 3
